0d3bac31d0998f2746775a0b0b0f1d8838ec8d14
надежда <rinstren@mail.ru>
2022-03-29 18:12:55 +0300

    Устранение ошибок

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ const getRandomInt = (min = 0, max = 0) => {
     throw new Error('Диапазон должен быть положительный!');
   }
   if (max < min) {
-    throw new Error('Минимальное значение не может превышать максимальное!')
+    throw new Error('Минимальное значение не может превышать максимальное!');
   }
 
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
 getRandomInt();
 
